3420e7589936a43d63f3ad996e964eea492e16ae
Tiffo <szuchinlin3@gmail.com>
2019-07-11 15:05:05 -0400

    btn style

--- client/src/components/LinkBtn/index.js
+++ client/src/components/LinkBtn/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 
 function LinkBtn(props) {
   return (
-    <button className="linkBtn btn btn-success" onClick={props.onClick}>
+    <button className="StylelinkBtn btn btn-success" onClick={props.onClick}>
       <span> Select
         <Link className="navbar-brand" to="RecipeSelect"></Link>
       </span>
